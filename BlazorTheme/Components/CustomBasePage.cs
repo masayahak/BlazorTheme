6fb0eb34ec63f7dd7e7886b3b0a9a6b4fbfaa7a3
masayahak <masayahak@outlook.jp>
2024-12-10 07:32:58 +0900

    修正

--- BlazorTheme/Components/CustomBasePage.cs
+++ BlazorTheme/Components/CustomBasePage.cs
@@ -8,17 +8,12 @@ namespace BlazorTheme.Components
         [Inject]
         protected ThemeService ThemeService { get; set; } = default!;
 
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-            await ThemeService.GetCurrentTheme();
-            await ThemeService.ApplyCurrentTheme();
-        }
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             // StreamRenderingの場合、OnAfterRenderは複数回実行される。
-            // 初回だけ実行するようにしてしまうと、<table>タグが生成されるまえに
-            // Applyしてしまう可能性があるので、冗長だがAfterRenderのたびに実行する。
+            // 初回だけ実行するようにしてしまうと、<table>タグが生成されるまえにだけ
+            // Applyする可能性があるので、冗長だがAfterRenderのたびに実行する。
+            await ThemeService.GetCurrentTheme();
             await ThemeService.ApplyCurrentTheme();
         }
     }
